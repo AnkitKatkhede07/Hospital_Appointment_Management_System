c9afd1377bb85852be10d56526fe1557ed910f3a
Ankit Katkhede <ankitkatkhede@gmail.com>
2026-05-30 00:55:30 +0530

    update railway public db url

--- src/main/java/com/db/DBConnection.java
+++ src/main/java/com/db/DBConnection.java
@@ -16,12 +16,15 @@ public class DBConnection {
                 Class.forName("com.mysql.cj.jdbc.Driver");
 
                 String url =
-                    "jdbc:mysql://kodama.proxy.rlwy.net:40021/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+                        "jdbc:mysql://nozomi.proxy.rlwy.net:53176/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
                 String user = "root";
-                String password = "XlXTHUwtHdnnmkVIsyWLSJIWECdGniZq";
+
+                String password = "UMUCrsMaiPoehDvHWuAviVjACqbjQRtp";
 
                 con = DriverManager.getConnection(url, user, password);
+
+                System.out.println("Railway MySQL Connected Successfully...");
             }
 
         } catch (Exception e) {
